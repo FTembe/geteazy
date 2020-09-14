5d42b894abc53ed3c51a81e3487eb7cf1af5e60b
khyto94 <franciscojtembe@gmail.com>
2020-09-14 16:49:02 +0200

    Updates

---
+++ precache-manifest.694daf5002592ce0247e52de1f4a9bca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca17e069499ecd3320901a1e4b0ccb2",
+    "url": "/geteazy/index.html"
+  },
+  {
+    "revision": "c10e77f629dc85feb0fa",
+    "url": "/geteazy/static/css/main.4e936bc5.chunk.css"
+  },
+  {
+    "revision": "448cb0564ae14ff4b633",
+    "url": "/geteazy/static/js/2.e3e9c00c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/geteazy/static/js/2.e3e9c00c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10e77f629dc85feb0fa",
+    "url": "/geteazy/static/js/main.e194e48f.chunk.js"
+  },
+  {
+    "revision": "fd87b6477cad3b8a37b0",
+    "url": "/geteazy/static/js/runtime-main.999776d0.js"
+  },
+  {
+    "revision": "3b41dc21539db5c75cb12ca65e91c3de",
+    "url": "/geteazy/static/media/bg.3b41dc21.png"
+  },
+  {
+    "revision": "eda1b0082ee1f8f0179df4ceb6b57751",
+    "url": "/geteazy/static/media/facebook.eda1b008.svg"
+  },
+  {
+    "revision": "9408234ee19fd3b5f022882baf81ad6f",
+    "url": "/geteazy/static/media/google.9408234e.svg"
+  },
+  {
+    "revision": "40a13bbc9c367c2bda9afced0da19638",
+    "url": "/geteazy/static/media/logo.40a13bbc.png"
+  }
+]);
